adea1e4a6fce3746d9a6ef37d158ef62c9dad99e
LuNgocDuc <lungocduc.horse@gmail.com>
2025-10-15 17:45:56 +0700

    install prettier, lint-stage, husky

--- src/index.js
+++ src/index.js
@@ -1,27 +1,30 @@
-const path = require('path')
-const express = require('express')
-const morgan = require('morgan')
-const app = express()
-const port = 3000
+const path = require('path');
+const express = require('express');
+const morgan = require('morgan');
+const app = express();
+const port = 3000;
 const { engine } = require('express-handlebars');
 
-const route = require('./routes')
+const route = require('./routes');
 
-app.use(express.static(path.join(__dirname, 'resources', 'public'))); 
+app.use(express.static(path.join(__dirname, 'resources', 'public')));
 
-//HTTP logger 
-app.use(morgan('combined'))
+//HTTP logger
+app.use(morgan('combined'));
 
 // Template engine
-app.engine('.hbs', engine({
-  extname: '.hbs'
-}));
+app.engine(
+    '.hbs',
+    engine({
+        extname: '.hbs',
+    }),
+);
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 
 //Routes init
-route(app);
+route(        app);
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+          app.listen(port, () => {
+              console.log(`Example app listening on port ${port}`);
+          });
