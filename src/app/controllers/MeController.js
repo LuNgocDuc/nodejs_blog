23737d7487faffe52b8057e23a4263e565864f6a
LuNgocDuc <lungocduc.horse@gmail.com>
2025-10-21 15:43:22 +0700

    CRUD + soft delete

---
+++ src/app/controllers/MeController.js
@@ -0,0 +1,27 @@
+const Course = require('../models/Course')
+const {mutipleMongooseToObject} = require('../../util/mongoose')
+
+class MeController {
+    // [GET] /me/stored/courses
+    storedCourses(req, res, next) {
+        Promise.all([Course.find({}), Course.findDeleted({})])
+            .then(([courses, deletedCount]) =>
+                res.render('me/stored-courses', {
+                    courses: mutipleMongooseToObject(courses),
+                    deletedCount: deletedCount.filter(course => course.deleted).length
+                }),
+            )
+            .catch(next);
+    }
+
+    // [GET] /me/stored/courses
+    trashCourses(req, res, next) {
+        Course.findWithDeleted({deleted: true})
+            .then(courses => res.render('me/trash-courses', {
+                courses: mutipleMongooseToObject(courses)
+            }))
+            .catch(next)
+    }
+}
+
+module.exports = new MeController();
--- src/index.js
+++ src/index.js
@@ -4,10 +4,20 @@ const morgan = require('morgan');
 const app = express();
 const port = 3000;
 const { engine } = require('express-handlebars');
+const methodOverride = require('method-override')
 
 const route = require('./routes');
+const db = require('./config/db');
+
+// Connect to DB
+db.connect();
 
 app.use(express.static(path.join(__dirname, 'resources', 'public')));
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+app.use(methodOverride('_method'))
 
 //HTTP logger
 app.use(morgan('combined'));
@@ -17,14 +27,17 @@ app.engine(
     '.hbs',
     engine({
         extname: '.hbs',
+        helpers: {
+            sum: (a, b) => a + b 
+        }
     }),
 );
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
+app.use(express.static(__dirname + '/public'))
 
 //Routes init
-route(        app);
-
-          app.listen(port, () => {
-              console.log(`Example app listening on port ${port}`);
-          });
+route(app);
+    app.listen(port, () => {
+        console.log(`Example app listening on port ${port}`);
+    });
